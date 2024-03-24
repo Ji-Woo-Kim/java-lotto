4b88c2c79ae11c5834143324d2a892dd0e55dae1
jiwoo <ziuoo520@naver.com>
2024-03-24 22:59:04 +0900

    first commit_로또 번호 생성

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
@@ -17,4 +19,20 @@ public class Lotto {
     }
 
     // TODO: 추가 기능 구현
+    public static LottoTicket generateTicket() {
+        List<Integer> ticketNumbers = new ArrayList<>();
+        for (int i = 1; i <= 45; i++) {
+            ticketNumbers.add(i);
+        }
+        Collections.shuffle(ticketNumbers);
+        ticketNumbers = ticketNumbers.subList(0, 6);
+        Collections.sort(ticketNumbers);
+        return new LottoTicket(ticketNumbers);
+    }
+
+    public static void main(String[] args) {
+        LottoTicket ticket = Lotto.generateTicket();
+        List<Integer> numbers = ticket.getNumbers();
+        System.out.println("로또 번호: " + numbers);
+    }
 }
